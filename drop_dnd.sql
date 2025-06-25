48def9a3f64e432dda4557488eb350ecb0d95562
corbinrix42 <corbinrix42@gmail.com>
2025-06-25 13:08:43 -0500

    Add files via upload

---
+++ drop_dnd.sql
@@ -0,0 +1,17 @@
+DROP TABLE Races cascade constraints;
+
+DROP TABLE Campaign cascade constraints; 
+
+DROP TABLE Classes cascade constraints;
+
+DROP TABLE Players cascade constraints;
+
+DROP TABLE Stat cascade constraints;
+
+DROP TABLE Characters cascade constraints;
+
+DROP TABLE Char_Statistics cascade constraints;
+
+DROP TABLE Race_Stat cascade constraints;
+
+COMMIT;
